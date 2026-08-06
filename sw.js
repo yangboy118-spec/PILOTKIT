f14a3914d7b750c012aaa65928d1b4f2d45f8538
yangboy118-spec <yangboy118@gmail.com>
2026-08-06 08:24:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pilog-v0.8.1';
-const ASSETS=['./','./index.html?v=081','./manifest.webmanifest?v=081','./icon.svg?v=081','./world-map.svg?v=081'];
+const CACHE='pilog-v0.9';
+const ASSETS=['./','./index.html?v=090','./manifest.webmanifest?v=090','./icon.svg?v=090','./world-map.svg?v=090','./a350-sample.jpeg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
-self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html?v=081')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html?v=090')));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
